418dae9830814ef80db1dc71ec7dd83f7f29a900
Footagesus <alisher.bahitbek.discord2@gmail.com>
2025-11-03 22:21:26 +0500

    fix: windui raw link

--- main_example.lua
+++ main_example.lua
@@ -15,7 +15,7 @@ do
     if ok then
         WindUI = result
     else 
-        WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+        WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/dist/main.lua"))()
     end
 end
 
