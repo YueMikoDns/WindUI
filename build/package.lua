0bfe714939f3367ac783228100fcfe6de726549b
Footagesus <alisher.bahitbek.discord2@gmail.com>
2025-11-14 23:22:20 +0500

    chore: bump version to 1.6.62

--- build/package.lua
+++ build/package.lua
@@ -3,7 +3,7 @@
 return [[
 {
     "name": "windui",
-    "version": "1.6.61",
+    "version": "1.6.62",
     "main": "./dist/main.lua",
     "repository": "https://github.com/Footagesus/WindUI",
     "discord": "https://discord.gg/ftgs-development-hub-1300692552005189632",
